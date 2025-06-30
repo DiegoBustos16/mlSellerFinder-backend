b70a301fedd20f6c432887185b4f01f5f275deb8
DiegoBustos16 <diegombustos16@gmail.com>
2025-06-30 16:23:46 -0300

    Initial Commit

---
+++ src/main/java/com/DiegoBustos/mlSellerFinder_backend/MlSellerFinderBackendApplication.java
@@ -0,0 +1,13 @@
+package com.DiegoBustos.mlSellerFinder_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlSellerFinderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlSellerFinderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DiegoBustos/mlSellerFinder_backend/MlSellerFinderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DiegoBustos.mlSellerFinder_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlSellerFinderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
